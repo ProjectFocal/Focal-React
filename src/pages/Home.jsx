adbf46aa821dea42ed6dd272159194b606d8461e
trulyNathan2 <nathanaruna07@gmail.com>
2022-08-20 22:37:35 -0400

    UploadContent Section Start

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,158 +1,105 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from 'react';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Avatar from '@mui/joy/Avatar';
-import Box from '@mui/joy/Box';
-import Card from '@mui/joy/Card';
-import CardOverflow from '@mui/joy/CardOverflow';
-import Link from '@mui/joy/Link';
-import IconButton from '@mui/joy/IconButton';
-import Input from '@mui/joy/Input';
-import Typography from '@mui/joy/Typography';
-import MoreHoriz from '@mui/icons-material/MoreHoriz';
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import ModeCommentOutlined from '@mui/icons-material/ModeCommentOutlined';
-import SendOutlined from '@mui/icons-material/SendOutlined';
-import Face from '@mui/icons-material/Face';
-import BookmarkBorderRoundedIcon from '@mui/icons-material/BookmarkBorderRounded';
-import { Paper } from '@mui/material';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
 
-//Demo All of this content will be handled in the Post Handler src/components/UsersContent
-
-export default function InstagramPost() {
+export default function AccountMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
-    <div className='padding'>
-      <div className='center'>
-    <Paper elevation={6} sx={{
-      minWidth: 700,
-      maxWidth: 700,
-
-    }} >
-    <Card
-        sx={{
-        '--Card-radius': (theme) => theme.vars.radius.xs,
-      }}
-    >
-      <Box sx={{ display: 'flex', alignItems: 'center', pb: 1.5, gap: 1 }}>
-        <Box
-          sx={{
-            position: 'relative',
+    <React.Fragment>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <Typography sx={{ minWidth: 100 }}>Contact</Typography>
+        <Typography sx={{ minWidth: 100 }}>Profile</Typography>
+        <Tooltip title="Account settings">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
             '&:before': {
               content: '""',
+              display: 'block',
               position: 'absolute',
               top: 0,
-              left: 0,
-              bottom: 0,
-              right: 0,
-              m: '-2px',
-              borderRadius: '50%',
-              background:
-                'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)',
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
             },
-          }}
-        >
-          <Avatar
-            size="sm"
-            src="/static/logo.png"
-            sx={{ p: 0.5, border: 'px solid', borderColor: 'background.body' }}
-          />
-        </Box>
-        <Typography fontWeight="lg">MUI</Typography>
-        <IconButton variant="plain" color="neutral" size="sm" sx={{ ml: 'auto' }}>
-          <MoreHoriz />
-        </IconButton>
-      </Box>
-      <CardOverflow>
-        <AspectRatio >
-          <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/05/c1/ff/28/caption.jpg?w=700&h=500&s=1" alt="" />
-        </AspectRatio>
-      </CardOverflow>
-      <Box sx={{ display: 'flex', alignItems: 'center', mx: -1, my: 1 }}>
-        <Box sx={{ width: 0, display: 'flex', gap: 0.5 }}>
-          <IconButton variant="plain" color="neutral" size="sm">
-            <FavoriteBorder />
-          </IconButton>
-          <IconButton variant="plain" color="neutral" size="sm">
-            <ModeCommentOutlined />
-          </IconButton>
-          <IconButton variant="plain" color="neutral" size="sm">
-            <SendOutlined />
-          </IconButton>
-        </Box>
-        <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5, mx: 'auto' }}>
-          {[...Array(5)].map((_, index) => (
-            <Box
-              key={index}
-              sx={{
-                borderRadius: '50%',
-                width: `max(${6 - index}px, 3px)`,
-                height: `max(${6 - index}px, 3px)`,
-                bgcolor: index === 0 ? 'primary.solidBg' : 'background.level3',
-              }}
-            />
-          ))}
-        </Box>
-        <Box sx={{ width: 0, display: 'flex', flexDirection: 'row-reverse' }}>
-          <IconButton variant="plain" color="neutral" size="sm">
-            <BookmarkBorderRoundedIcon />
-          </IconButton>
-        </Box>
-      </Box>
-      <Link
-        component="button"
-        underline="none"
-        fontSize="sm"
-        fontWeight="lg"
-        textColor="text.primary"
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
-        8.1M Likes
-      </Link>
-      <Typography fontSize="sm">
-        <Link
-          component="button"
-          color="neutral"
-          fontWeight="lg"
-          textColor="text.primary"
-        >
-          UserName:
-        </Link>{' '}
-        The React component library you always wanted
-      </Typography>
-      <Link
-        component="button"
-        underline="none"
-        fontSize="sm"
-        startDecorator="…"
-        sx={{ color: 'text.tertiary' }}
-      >
-        more
-      </Link>
-      <Link
-        component="button"
-        underline="none"
-        fontSize="10px"
-        sx={{ color: 'text.tertiary', my: 0.5 }}
-      >
-        2 DAYS AGO
-      </Link>
-      <CardOverflow sx={{ p: 'var(--Card-padding)', display: 'flex' }}>
-        <IconButton size="sm" variant="plain" color="neutral" sx={{ ml: -1 }}>
-          <Face />
-        </IconButton>
-        <Input
-          variant="plain"
-          size="sm"
-          placeholder="Add a comment…"
-          sx={{ flexGrow: 1, mr: 1, '--Input-focusedThickness': '0px' }}
-        />
-        <Link disabled underline="none" role="button">
-          Comment
-        </Link>
-      </CardOverflow>
-    </Card>
-    </Paper>
-    </div>
-    </div>
-    
+        <MenuItem>
+          <Avatar /> Profile
+        </MenuItem>
+        <MenuItem>
+          <Avatar /> My account
+        </MenuItem>
+        <Divider />
+        <MenuItem>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </React.Fragment>
   );
 }
--- src/pages/UploadContent.jsx
+++ src/pages/UploadContent.jsx
@@ -1,99 +1,205 @@
-import { useState } from 'react';
-import Page from '../components/Page';
-import React, { Component }  from 'react';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import SkipPreviousIcon from '@mui/icons-material/SkipPrevious';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
-import { Chip } from '@mui/material';
-import PropTypes from 'prop-types';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import PersonIcon from '@mui/icons-material/Person';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import Button from '@mui/material/Button';
-import DeleteIcon from '@mui/icons-material/Delete';
-import SendIcon from '@mui/icons-material/Send';
-import Stack from '@mui/material/Stack';
+import * as React from 'react';
+import Box from '@mui/joy/Box';
+import Chip from '@mui/joy/Chip';
+import Card from '@mui/joy/Card';
+import CardOverflow from '@mui/joy/CardOverflow';
+import Sheet from '@mui/joy/Sheet';
+import Typography from '@mui/joy/Typography';
+import Button from '@mui/joy/Button';
+import IconButton from '@mui/joy/IconButton';
+import AspectRatio from '@mui/joy/AspectRatio';
+import ListDivider from '@mui/joy/ListDivider';
+import Avatar from '@mui/joy/Avatar';
+import { Paper } from '@mui/material';
+import ImageIcon from '@mui/icons-material/Image';
+import TextField from '@mui/material/TextField';
+import Alert from '@mui/material/Alert';
+import { Navigate } from "react-router-dom";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button2 from '@mui/material/Button';
+import Chip2 from '@mui/material/Chip';
+import IconButton2 from '@mui/material/IconButton';
 
 
+// Icons import
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
+import ForwardToInboxRoundedIcon from '@mui/icons-material/ForwardToInboxRounded';
+import FolderIcon from '@mui/icons-material/Folder';
 
-
-var status = String("Online")
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
-}
-
-
-
-export default function About() {
-  const [openFilter, setOpenFilter] = useState(false);
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  const handleOpenFilter = () => {
-    setOpenFilter(true);
-  };
-
-
-  const theme = useTheme(); 
+export default function UploadContent() {
+  const [value, setValue] = React.useState('');
   
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState('paper');
+
+  const handleClickOpen = (scrollType) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+  const descriptionElementRef = React.useRef(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
 
   return (
-    
     <>
-
-     <Stack direction="row" spacing={2}>
-       <Button variant="outlined" startIcon={<DeleteIcon />}>
-        Delete
-      </Button>
-    </Stack>
-   
-      </>
-      
-
-
-      
     
+
+    <div className='padding'>
+      <Paper elevation={6}>
+        <Sheet
+
+          sx={{
+            minHeight: 500,
+            borderRadius: 'sm',
+            p: 2,
+            mb: 3,
+            bgcolor: 'background.componentBg',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              flexWrap: 'wrap',
+              gap: 2,
+            }}
+          >
+            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Avatar
+                src="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+                srcSet="https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"
+                sx={{ borderRadius: 'sm' }} />
+              <Box sx={{ ml: 2 }}>
+                <Typography level="body2" textColor="text.primary" mb={0.5}>
+                  USERNAME
+                </Typography>
+                <Typography level="body3" textColor="text.tertiary">
+                  
+                </Typography>
+              </Box>
+            </Box>
+            <Box
+              sx={{ display: 'flex', height: '32px', flexDirection: 'row', gap: 1.5 }}
+            >
+              <Button2 variant="outlined"  >
+               Post
+              </Button2>
+              
+              <IconButton2 color="primary" aria-label="upload picture" component="label">
+                <input hidden accept="image/*" type="file" />
+                <ImageIcon />
+              </IconButton2>
+            </Box>
+          </Box>
+
+          <ListDivider component="hr" sx={{ mt: 2 }} />
+          <ListDivider component="hr" />
+
+          <TextField fullWidth label="Description" id="fullWidth" multiline
+            maxRows={4}
+            value={value}
+            onChange={handleChange}
+            variant="filled" />
+
+          <Typography level="body2" textColor="text.secondary" mt={2} mb={2}>
+
+          
+                
+
+            <Alert severity="warning">Please Comply With Our <Chip2 size="small" label="Terms Of Service" variant="outlined" onClick={handleClickOpen('paper')} /> Before Posting.</Alert>
+          </Typography>
+          <ListDivider component="hr" />
+          <Typography
+            level="body2"
+            fontWeight="md"
+            textColor="text.primary"
+            mt={2}
+            mb={2}
+          >
+            Attachments
+          </Typography>
+          <Box
+            sx={(theme) => ({
+              display: 'flex',
+              flexWrap: 'wrap',
+              gap: 2,
+              '& > div': {
+                boxShadow: 'none',
+                '--Card-padding': '0px',
+                '--Card-radius': theme.vars.radius.sm,
+              },
+            })}
+          >
+            <Card>
+              <AspectRatio ratio="1" sx={{ minWidth: 80 }}>
+                <img
+                  src="https://images.unsplash.com/photo-1527549993586-dff825b37782?auto=format&fit=crop&w=2370"
+                  alt="Yosemite National Park" />
+              </AspectRatio>
+            </Card>
+            <Card >
+              <AspectRatio ratio="1" sx={{ minWidth: 80 }}>
+                <img
+                  src="https://images.unsplash.com/photo-1532614338840-ab30cf10ed36?auto=format&fit=crop&w=2370"
+                  alt="Yosemite National Park" />
+              </AspectRatio>
+            </Card>
+      
+          </Box>
+        </Sheet>
+      </Paper>
+    </div>
+
+
+    <div>
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogTitle id="scroll-dialog-title">Terms of Service</DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+          <DialogContentText
+            id="scroll-dialog-description"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+            {[...new Array(10)]
+              .map(
+                () => `Terms of service (also known as terms of use and terms and conditions, commonly abbreviated as TOS or ToS, ToU or T&C) are the legal agreements between a service provider and a person who wants to use that service. The person must agree to abide by the terms of service in order to use the offered service.`,
+              )
+              .join('\n')}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          
+          <Button onClick={handleClose}>Accept</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+    </>
   );
 }
